940050b71dca0a7fe98ae6d18e4c8e5aa65f5958
mbyhphat <22521067@gm.uit.edu.vn>
2025-02-26 20:17:13 +0700

    Create file

---
+++ store_index.py
@@ -0,0 +1,37 @@
+from src.utility import load_pdf_file, text_split, load_embeddings
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+extracted_data = load_pdf_file(data="data/")
+text_chunks = text_split(extracted_data)
+embeddings = load_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "mlbot"
+
+pc.create_index(
+    name=index_name,
+    dimension=1536, 
+    metric="cosine", 
+    spec=ServerlessSpec(
+        cloud="aws", 
+        region="us-east-1"
+    ) 
+) 
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings
+)
