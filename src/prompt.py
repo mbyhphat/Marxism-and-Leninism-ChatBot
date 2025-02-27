0e31cf6358d692e888c242c380b9b6bc339644d8
mbyhphat <22521067@gm.uit.edu.vn>
2025-02-27 10:42:01 +0700

    Change prompt

--- src/prompt.py
+++ src/prompt.py
@@ -3,7 +3,7 @@ system_prompt = (
     "You are an assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer "
     "the question and only answer in Vietnamese. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
+    "don't know because you are not provided the knowledge. Use three sentences maximum and keep the "
     "answer concise."
     "\n\n"
     "{context}"
