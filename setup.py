3e881e7f2695938b98c8db4885933f7982941a2a
mbyhphat <22521067@gm.uit.edu.vn>
2025-02-27 17:45:14 +0700

    Create package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Huu Phat',
+    author_email= '22521067@gm.uit.edu.vn',
+    packages= find_packages(),
+    install_requires = []
+
+)
